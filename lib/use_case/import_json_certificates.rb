251aa63f2a1b61e05e52d34f18b0366f5a0feaa4
Barry Halper <barryhalper@gmail.com>
2021-05-20 16:42:49 +0100

    EPBR-1279 Updated json import to loop files and save the attributes

--- lib/use_case/import_json_certificates.rb
+++ lib/use_case/import_json_certificates.rb
@@ -1,17 +1,30 @@
 module UseCase
   class ImportJsonCertificates
-    attr_accessor :file_gateway
+    attr_accessor :file_gateway, :assessment_attribute_gateway
 
-    def initialize(file_gateway)
+    def initialize(file_gateway, assessment_gateway)
       @file_gateway = file_gateway
+      @assessment_attribute_gateway = assessment_gateway
     end
 
     def execute
       arr = []
       files = @file_gateway.read
-      files.each { |f| arr << JSON.parse(File.read(f)) }
+      files.each do |f|
+        certificate = JSON.parse(File.read(f))
+        save_attribute_data(certificate)
+        arr << certificate
+      end
 
       arr
+    end
+
+  private
+
+    def save_attribute_data(certificate)
+      certificate.each do |_key, _value|
+        @assessment_attribute_gateway.add_attribute_value("001", "a", "c")
+      end
     end
   end
 end
--- spec/unit/use_case/import_json_certificates_spec.rb
+++ spec/unit/use_case/import_json_certificates_spec.rb
@@ -1,8 +1,14 @@
 describe UseCase::ImportJsonCertificates do
   context "execute import use case" do
-    subject { described_class.new(gateway) }
+    subject do
+      described_class.new(directory_gateway, assessment_attribute_gateway)
+    end
 
-    let(:gateway) { instance_double(Gateway::JsonCertificates) }
+    let(:directory_gateway) { instance_double(Gateway::JsonCertificates) }
+
+    let(:assessment_attribute_gateway) do
+      instance_double(Gateway::AssessmentAttributesGateway)
+    end
 
     before do
       path = File.join Dir.pwd, "spec/fixtures/json_export/"
@@ -10,11 +16,19 @@ describe UseCase::ImportJsonCertificates do
         Dir
           .glob(File.join(path, "**", "*"))
           .select { |file| File.file?(file) && File.extname(file) == ".json" }
-      allow(gateway).to receive(:read).and_return(files)
+      allow(directory_gateway).to receive(:read).and_return(files)
+
+      allow(assessment_attribute_gateway).to receive(:add_attribute_value)
     end
 
     it "returns the files from the directory" do
       expect(subject.execute.count).to eq(3)
+    end
+
+    it "returns an array of hashes for each json file" do
+      expect(subject.execute[0]).to be_a(Hash)
+      expect(subject.execute[1]).to be_a(Hash)
+      expect(subject.execute[2]).to be_a(Hash)
     end
   end
 end
