965a977dc60752a031e38f72d805f3870c50e2a5
Barry Halper <barryhalper@gmail.com>
2021-05-24 12:49:56 +0100

    EPBR-1320 Updated test to include all 3 json certifcate types

--- spec/unit/use_case/import_json_certificates_spec.rb
+++ spec/unit/use_case/import_json_certificates_spec.rb
@@ -10,7 +10,7 @@ describe UseCase::ImportJsonCertificates do
       instance_double(Gateway::AssessmentAttributesGateway)
     end
 
-    let(:files) do
+    let!(:files) do
       path = File.join Dir.pwd, "spec/fixtures/json_export/"
       Dir
         .glob(File.join(path, "**", "*"))
@@ -27,10 +27,6 @@ describe UseCase::ImportJsonCertificates do
       expect(files.count).to eq(3)
     end
 
-    it "returns a hash for each attribute" do
-      expect(subject.execute).to be_truthy
-    end
-
     context "when use case uses the actual attribute gateway" do
       before do
         use_case =
@@ -42,6 +38,13 @@ describe UseCase::ImportJsonCertificates do
       end
 
       let!(:imported_data) do
+        use_case =
+          UseCase::ImportJsonCertificates.new(
+            directory_gateway,
+            Gateway::AssessmentAttributesGateway.new,
+          )
+        use_case.execute
+
         ActiveRecord::Base.connection.exec_query(
           'SELECT aa.*, aav.attribute_value,  aav.attribute_value_int, aav.attribute_value_float, aav.assessment_id
             FROM assessment_attribute_values aav
@@ -49,19 +52,40 @@ describe UseCase::ImportJsonCertificates do
         )
       end
 
-      it "the assessment_attribute_values table has data for one assessment" do
+      it "the assessment_attribute_values table has data for each of the 3 test assessments" do
         expect(
           imported_data.group_by { |item| item["assessment_id"] }.count,
         ).to eq(3)
       end
 
-      it "the assessment_attribute_values table has data for  asset_rating" do
+      it "the assessment_attribute_values has one entry for each type of assessment" do
+        select =
+          imported_data.select do |item|
+            item["attribute_name"] == "type_of_assessment"
+          end
+        expect(select.map { |item| item["attribute_value"] }).to eq(
+          %w[RdSAP CEPC SAP],
+        )
+      end
+
+      it "the assessment_attribute_values table has CEPC data for asset_rating" do
         arr =
           imported_data.select do |result|
             result["attribute_name"] == "asset_rating"
           end
         expect(arr[0]["attribute_value"]).to eq("80")
         expect(arr[0]["parent_name"]).to eq(nil)
+        expect(arr[0]["attribute_value_int"]).to eq(80)
+      end
+
+      it "returns SAP data for current_carbon_emission" do
+        arr =
+          imported_data.select do |result|
+            result["attribute_name"] == "current_carbon_emission"
+          end
+        expect(arr[0]["attribute_value"]).to eq("2.4")
+        expect(arr[0]["parent_name"]).to eq(nil)
+        expect(arr[0]["attribute_value_float"]).to eq(2.4)
       end
 
       it "the attribute table should contain one hash for the town with a parent of address" do
